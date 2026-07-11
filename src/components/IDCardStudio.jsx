b23bb1b8acedd783c5094546b42a4411d6d3f71e
Asad16538 <120198263+Asad16538@users.noreply.github.com>
2026-07-11 22:54:27 +0530

    Update IDCardStudio.jsx

--- src/components/IDCardStudio.jsx
+++ src/components/IDCardStudio.jsx
@@ -12,10 +12,10 @@ const IDCardStudio = () => {
     // ============================================
     const [classes] = useState([
         'Nursery', 'LKG', 'UKG',
-        'Class 1st', 'Class 2nd', 'Class 3rd', 'Class 4th', 'Class 5th', 
-        'Class 6th', 'Class 7th', 'Class 8th', 'Class 9th', 'Class 10th',
-        'Class 11th (PCB)','Class 11th (PCM)', 'Class 11th (Commerce)', 'Class 11th (Arts)',
-        'Class 12th (PCB)','Class 12th (PCM)', 'Class 12th (Commerce)', 'Class 12th (Arts)'
+        'Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 
+        'Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10',
+        'Class 11 (PCB)','Class 11 (PCM)', 'Class 11 (Commerce)', 'Class 11 (Arts)',
+        'Class 12 (PCB)','Class 12 (PCM)', 'Class 12 (Commerce)', 'Class 12 (Arts)'
     ]);
     
     const [selectedClass, setSelectedClass] = useState('');
